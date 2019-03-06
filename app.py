83934a9f970fc06eb58e0a8b8aed7f2e5ac1ae54
NEsfandiari <nesfandiari37@gmail.com>
2019-03-06 14:50:51 -0800

    flask ap start

--- app.py
+++ app.py
@@ -1,5 +1,14 @@
-from links import scrape_all_ribbonfarm
-from links import counter
+from flask import Flask
+import csv
+import json
+import pdb
+app = Flask(__name__)
 
-# scrape_all_ribbonfarm()
-counter()
+
+@app.route('/')
+def return_data():
+    with open('links.csv', 'r') as f:
+        fieldnames = ("Link", "Article", "Author", "Date")
+        data = csv.DictReader(f, fieldnames=fieldnames)
+        data = {"data": [row for row in data]}
+        return json.dumps(data)
---
+++ scrape_launch.py
@@ -0,0 +1,5 @@
+from links import scrape_all_ribbonfarm
+from links import counter
+
+# scrape_all_ribbonfarm()
+counter()
